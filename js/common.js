73bd6b1d4a60db4be80568bfbe583fd41961f47d
Destroy <2087953253@qq.com>
2022-02-16 15:40:43 +0800

    furry

--- js/common.js
+++ js/common.js
@@ -2,15 +2,20 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer({alpha:true, antialias:true});
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth/2, window.innerHeight/2);
 //document.body.appendChild(renderer.domElement);
 document.getElementsByClassName("three")[0].appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0xFFFF00 } );
-material.lights = true;
+var material = new THREE.MeshBasicMaterial( { color: 0xFFDD00 } );
+//var material = new THREE.MeshStandardMaterial({color: 0xFFFF00});
+
+// var pointLight = new THREE.PointLight( 0xFFFF00, 1, 0 );
+// pointLight.position.set( 0, 5, 0 );
+// scene.add( pointLight );
 
 var cube = new THREE.Mesh( geometry, material );
+
 scene.add(cube);
 
 camera.position.z = 5;
